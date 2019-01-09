549873197c3525cea09014b420f9d8c55453280b
Nidhi <gowripatel86@gmail.com>
2019-01-09 16:29:06 +0530

    Initial commit

---
+++ src/jan28/DemoA.java
@@ -0,0 +1,11 @@
+package jan28;
+
+public class DemoA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+
+	}
+
+}
---
+++ src/jan28/DemoB.java
@@ -0,0 +1,12 @@
+package jan28;
+
+public class DemoB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		
+
+	}
+
+}
